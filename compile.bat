5fe60e3fec10c88f84bb8844a2395a3019cc3eab
mks-a <8500434+mks-a@users.noreply.github.com>
2018-03-18 18:14:57 +0200

    20180318 2
    20180318 2

--- compile.bat
+++
@@ -1,9 +0,0 @@
-:: -g generate debug information
-:: -c compile only
-cls
-"D:\Arduino\arduino_1.8.3\hardware\tools\avr/bin/avr-gcc" -c -g -Os -w -fpermissive -fno-exceptions -ffunction-sections -fdata-sections -fno-threadsafe-statics -MMD -flto -mmcu=atmega328p -DF_CPU=16000000L -DARDUINO=10801 -DARDUINO_AVR_UNO -DARDUINO_ARCH_AVR .\code\main.c -o tmp\main.o
-"D:\Arduino\arduino_1.8.3\hardware\tools\avr/bin/avr-gcc" -c -g -Os -w -fpermissive -fno-exceptions -ffunction-sections -fdata-sections -fno-threadsafe-statics -MMD -flto -mmcu=atmega328p -DF_CPU=16000000L -DARDUINO=10801 -DARDUINO_AVR_UNO -DARDUINO_ARCH_AVR .\timer1\timer1.c -o tmp\timer1.o
-"D:\Arduino\arduino_1.8.3\hardware\tools\avr/bin/avr-gcc" -c -g -Os -w -fpermissive -fno-exceptions -ffunction-sections -fdata-sections -fno-threadsafe-statics -MMD -flto -mmcu=atmega328p -DF_CPU=16000000L -DARDUINO=10801 -DARDUINO_AVR_UNO -DARDUINO_ARCH_AVR .\twi\twi.c -o tmp\twi.o
-"D:\Arduino\arduino_1.8.3\hardware\tools\avr/bin/avr-gcc" -mmcu=atmega328p -o main.elf tmp\main.o tmp\bmp085.o tmp\timer1.o tmp\twi.o -lm -emain
-"D:\Arduino\arduino_1.8.3\hardware\tools\avr/bin/avr-objcopy" -O ihex -R .eeprom  main.elf main.hex
-
